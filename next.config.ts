6bed51547dc7c5323782223c209233d7eb74852c
Muhammad Adeel <muhammad_adeel91@yahoo.com>
2026-06-21 13:25:08 +0200

    chore: added standalone project run on render

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from 'next';
+import path from 'path';
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ['@mysten/walrus-wasm', '@mysten/walrus'],
+  output: 'standalone',
+  outputFileTracingRoot: path.join(__dirname, './'),
 };
 
 export default nextConfig;
